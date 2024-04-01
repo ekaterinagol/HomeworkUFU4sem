285ef1def581ab4fe9fd3c007a9df6fb2fe54432
golunkater <katerinagolunova@gmail.com>
2024-04-01 22:30:16 +0500

    2 try

--- BundleStruct/BundleStruct/Bundle.cs
+++ BundleStruct/BundleStruct/Bundle.cs
@@ -8,9 +8,20 @@ namespace BundleStruct
 {
     public class Bundle
     {
-        //public BanknoteType Banknote;
         public int Count { get; set; }
         public int Sum { get; set; }
 
+        int bancknote;
+        public int Banknote
+        {
+            get => bancknote;
+            set
+            {
+                if (value==1 || value == 2 || value == 5 || value == 10 || value == 50 || value == 100 || value == 200 || value == 500 || value == 1000 || value == 2000 || value == 5000)
+                    bancknote = value;
+
+                throw new ArgumentException("Номинал банкноты может принимать только определенные значения");
+            }
+        }
     }
 }
